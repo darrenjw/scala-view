9d78971969426a7b169f0dde9acf82bb14ca4523
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-08-17 22:34:36 +0100

    added a template for testing - no actual tests as yet

---
+++ src/test/scala/utils-tests.scala
@@ -0,0 +1,28 @@
+/*
+utils-tests.scala
+
+Some basic tests for the Utils
+
+I have no idea how to go about testing the GUI stuff...
+
+*/
+
+package scalaview
+
+import org.scalatest._
+
+class MyTestSuite extends FunSuite {
+
+  test("1+2=3") {
+    assert(1 + 2 === 3)
+  }
+
+}
+
+
+
+
+
+/* eof */
+
+
