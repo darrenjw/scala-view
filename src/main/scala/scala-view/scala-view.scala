c2aaa6a55fb9bef15be1cafce58c83ebb93a7c82
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-09-03 21:22:35 +0100

    option to save image frames in the api

--- src/main/scala/scala-view/scala-view.scala
+++ src/main/scala/scala-view/scala-view.scala
@@ -23,7 +23,7 @@ import scala.swing.event.{ButtonClicked, WindowClosing}
  * @param is Stream of BufferedImages to be animated on-screen
  * @param timerDelay Delay, in milliseconds, between rendering of frames in the stream
  */
-class SwingImageViewer(var is: Stream[BufferedImage], timerDelay: Int, autoStart: Boolean) {
+class SwingImageViewer(var is: Stream[BufferedImage], timerDelay: Int, autoStart: Boolean, saveFrames: Boolean) {
 
   def top = new MainFrame {
     title = "Swing Image Viewer"
@@ -31,9 +31,10 @@ class SwingImageViewer(var is: Stream[BufferedImage], timerDelay: Int, autoStart
     val stop = new Button { text = "Stop" }
     val panel = ImagePanel(is.head.getWidth, is.head.getHeight)
     val buttons = new BoxPanel(Orientation.Horizontal) {
-      contents+=start
-      contents+=stop
-      }
+      contents += start
+      contents += stop
+      border = Swing.EmptyBorder(5, 5, 5, 5)
+    }
     contents = new BoxPanel(Orientation.Vertical) {
       contents += buttons
       contents += panel
@@ -42,11 +43,14 @@ class SwingImageViewer(var is: Stream[BufferedImage], timerDelay: Int, autoStart
     peer.setDefaultCloseOperation(0)
     listenTo(start)
     listenTo(stop)
+    var frameCounter: Long = 0
     val timer = new javax.swing.Timer(timerDelay, Swing.ActionListener(e => {
       if (!is.isEmpty) {
+        frameCounter += 1
         panel.bi = is.head
         is = is.tail
         panel.repaint()
+        if (saveFrames) javax.imageio.ImageIO.write(panel.bi, "png", new java.io.File(f"siv-$frameCounter%06d.png"))
       }
     }))
     reactions += {
@@ -74,11 +78,12 @@ object SwingImageViewer {
    * @constructor Render the provided image stream on-screen
    * @param is Stream of images to be rendered on-screen
    * @param timerDelay Delay, in milliseconds, between frames of the image to be rendered on screen. Use the default for streams to be rendered as soon as each image is computed.
-    * @param autoStart Auto-start the animation without requiring a click on the Start button.
+   * @param autoStart Auto-start the animation without requiring a click on the Start button.
+   * @param saveFrames Save the image frames to sequentially numbered PNG files
    * @return In principle this returns an object, but in practice it is called purely for the side-effect of on-screen rendering.
    */
-  def apply(is: Stream[BufferedImage], timerDelay: Int = 1, autoStart: Boolean = false): SwingImageViewer = {
-    val siv = new SwingImageViewer(is, timerDelay, autoStart)
+  def apply(is: Stream[BufferedImage], timerDelay: Int = 1, autoStart: Boolean = false, saveFrames: Boolean = false): SwingImageViewer = {
+    val siv = new SwingImageViewer(is, timerDelay, autoStart, saveFrames)
     siv.top.visible = true
     siv
   }
@@ -91,7 +96,7 @@ object SwingImageViewer {
    * @return In principle this returns an object, but in practice it is called purely for the side-effect of on-screen rendering.
    */
   def apply(im: BufferedImage): SwingImageViewer = {
-    apply(Stream(im),autoStart=true)
+    apply(Stream(im), autoStart = true)
   }
 
 }
